f705deea2bdb24f8c3978e07f2023f5f5d39c8cb
fortishield <161459699+FortiShield@users.noreply.github.com>
2024-10-24 00:15:48 +0000

    Changes to be committed:

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
-require("kengine")
+require("khulnasoft")
+
---
+++ lua/khulnasoft/lazy/vimbegood.lua
@@ -0,0 +1,12 @@
+return {
+    "khulnasoft/vim-be-good",
+
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+    end
+}
+
+
