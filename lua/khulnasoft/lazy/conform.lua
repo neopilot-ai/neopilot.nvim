ef15c4281cb671780129019c15a6bf8220b4b8f4
gitworkflows <bdkhulnasoft@gmail.com>
2025-08-02 06:52:30 +0600

    feat(conform): add formatter configuration for multiple languages
    Signed-off-by: gitworkflows <bdkhulnasoft@gmail.com>

--- lua/khulnasoft/lazy/conform.lua
+++ lua/khulnasoft/lazy/conform.lua
@@ -1,12 +1,15 @@
 return {
-    'stevearc/conform.nvim',
-    opts = {},
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-            }
-        })
-    end
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      go = { "gofmt" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      elixir = { "mix" }
+    }
+  },
+  config = function(_, opts)
+    require("conform").setup(opts)
+  end,
 }
-
