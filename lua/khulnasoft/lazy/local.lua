f8f9afbb107f3b4f9c645072fbd28603ce80a40b
gitworkflows <bdkhulnasoft@gmail.com>
2025-08-02 06:55:51 +0600

    refactor(local): clean up local plugins and add the-stru
    - Reformatted and clarified all local plugin entries
    - Grouped keymaps and used local variables for clarity and maintainability
    - Added desc to keymaps for better discoverability
    - Included new plugin: the-stru
    - Removed unnecessary empty config functions
    - Improved overall code readability and structure
    
    Signed-off-by: gitworkflows <bdkhulnasoft@gmail.com>

--- lua/khulnasoft/lazy/local.lua
+++ lua/khulnasoft/lazy/local.lua
@@ -1,102 +1,77 @@
+-- Local plugin definitions for development and rapid iteration
 local local_plugins = {
-	--{
-	--    "vim-guys",
-	--    dir = "~/personal/vim-guys",
-	--    config = function()
-	--    end,
-	--},
-	--{
-	--    "cockpit",
-	--    dir = "~/personal/cockpit",
-	--    config = function()
-	--        require("cockpit")
-	--        vim.keymap.set("n", "<leader>ct", "<cmd>CockpitTest<CR>")
-	--        vim.keymap.set("n", "<leader>cr", "<cmd>CockpitRefresh<CR>")
-	--    end,
-	--},
+  -- The Stru (no config)
+  {
+    "the-stru",
+    dir = "~/personal/the-stru",
+  },
 
-	{
-		"streamer",
-		dir = "~/personal/eleven-streamer",
-		config = function()
-			vim.keymap.set("n", "<leader>er", function()
-				require("streamer").reload()
-			end)
-			vim.keymap.set("n", "<leader>es", function()
-				require("streamer.projects.unnamed_event").ue()
-			end)
-			vim.keymap.set("n", "<leader>en", function()
-				require("streamer.projects.unnamed_event").stop()
-			end)
-		end,
-	},
+  -- Streamer
+  {
+    "streamer",
+    dir = "~/personal/eleven-streamer",
+    config = function()
+      local streamer = require("streamer")
+      local unnamed_event = require("streamer.projects.unnamed_event")
+      vim.keymap.set("n", "<leader>er", streamer.reload, { desc = "Streamer: Reload" })
+      vim.keymap.set("n", "<leader>es", unnamed_event.ue, { desc = "Streamer: Start unnamed event" })
+      vim.keymap.set("n", "<leader>en", unnamed_event.stop, { desc = "Streamer: Stop unnamed event" })
+    end,
+  },
 
-	{
-		"caleb",
-		dir = "~/personal/caleb",
-		config = function() end,
-	},
-	{
-		"harpoon",
-		dir = "~/personal/harpoon",
-		config = function()
-			local harpoon = require("harpoon")
+  -- Caleb (no config)
+  {
+    "caleb",
+    dir = "~/personal/caleb",
+  },
 
-			harpoon:setup()
+  -- Harpoon
+  {
+    "harpoon",
+    dir = "~/personal/harpoon",
+    config = function()
+      local harpoon = require("harpoon")
+      harpoon:setup()
+      local list = harpoon:list()
+      local ui = harpoon.ui
 
-			vim.keymap.set("n", "<leader>A", function()
-				harpoon:list():prepend()
-			end)
-			vim.keymap.set("n", "<leader>a", function()
-				harpoon:list():add()
-			end)
-			vim.keymap.set("n", "<C-e>", function()
-				harpoon.ui:toggle_quick_menu(harpoon:list())
-			end)
+      -- Add/Prepend
+      vim.keymap.set("n", "<leader>A", function() list:prepend() end, { desc = "Harpoon: Prepend" })
+      vim.keymap.set("n", "<leader>a", function() list:add() end, { desc = "Harpoon: Add" })
 
-			vim.keymap.set("n", "<C-h>", function()
-				harpoon:list():select(1)
-			end)
-			vim.keymap.set("n", "<C-t>", function()
-				harpoon:list():select(2)
-			end)
-			vim.keymap.set("n", "<C-n>", function()
-				harpoon:list():select(3)
-			end)
-			vim.keymap.set("n", "<C-s>", function()
-				harpoon:list():select(4)
-			end)
-			vim.keymap.set("n", "<leader><C-h>", function()
-				harpoon:list():replace_at(1)
-			end)
-			vim.keymap.set("n", "<leader><C-t>", function()
-				harpoon:list():replace_at(2)
-			end)
-			vim.keymap.set("n", "<leader><C-n>", function()
-				harpoon:list():replace_at(3)
-			end)
-			vim.keymap.set("n", "<leader><C-s>", function()
-				harpoon:list():replace_at(4)
-			end)
-		end,
-	},
-	{
-		"vim-apm",
-		dir = "~/personal/vim-apm",
-		config = function()
-			--[[
-            local apm = require("vim-apm")
+      -- Quick menu
+      vim.keymap.set("n", "<C-e>", function() ui:toggle_quick_menu(list) end, { desc = "Harpoon: Quick Menu" })
 
-            apm:setup({})
-            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
-            --]]
-		end,
-	},
-	{
-		"vim-with-me",
-		dir = "~/personal/vim-with-me",
-		config = function() end,
-	},
+      -- Select by slot
+      vim.keymap.set("n", "<C-h>", function() list:select(1) end, { desc = "Harpoon: Select 1" })
+      vim.keymap.set("n", "<C-t>", function() list:select(2) end, { desc = "Harpoon: Select 2" })
+      vim.keymap.set("n", "<C-n>", function() list:select(3) end, { desc = "Harpoon: Select 3" })
+      vim.keymap.set("n", "<C-s>", function() list:select(4) end, { desc = "Harpoon: Select 4" })
+
+      -- Replace at slot
+      vim.keymap.set("n", "<leader><C-h>", function() list:replace_at(1) end, { desc = "Harpoon: Replace 1" })
+      vim.keymap.set("n", "<leader><C-t>", function() list:replace_at(2) end, { desc = "Harpoon: Replace 2" })
+      vim.keymap.set("n", "<leader><C-n>", function() list:replace_at(3) end, { desc = "Harpoon: Replace 3" })
+      vim.keymap.set("n", "<leader><C-s>", function() list:replace_at(4) end, { desc = "Harpoon: Replace 4" })
+    end,
+  },
+
+  -- Vim APM (example config, commented)
+  {
+    "vim-apm",
+    dir = "~/personal/vim-apm",
+    config = function()
+      -- local apm = require("vim-apm")
+      -- apm:setup({})
+      -- vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end, { desc = "APM: Toggle Monitor" })
+    end,
+  },
+
+  -- Vim With Me (no config)
+  {
+    "vim-with-me",
+    dir = "~/personal/vim-with-me",
+  },
 }
 
 return local_plugins
